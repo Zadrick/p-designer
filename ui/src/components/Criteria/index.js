56ceca50d73f92b04f3d969f8307b60e943e3369
Chynarbek uulu Nursultan <nursultan.chynarbek@timelysoft.net>
2022-03-09 11:25:55 +0600

    add 8 march

--- ui/src/components/Criteria/index.js
+++ ui/src/components/Criteria/index.js
@@ -1,9 +1,66 @@
+import React from'react'
+import './style.css'
 
 const Criteria = () => {
+    const [maxValue, setMaxValue] = React.useState(2.5)
+    const [minValue, setMinValue] = React.useState(0)
+    const [inputValue, setInputValue] = React.useState(1)
+
     return (
         <div className='sidebar__criteria'>
             <h2>Criteria Details</h2>
-            <button>Edit Optimization Criteria</button>
+            <div className='criteria criteria__border'>
+                <div className='criteria__item'>
+                   <div className='criteria__type'>Name:</div>
+                   <div className='criteria__value'>OEE</div>
+                </div>
+                <div className='criteria__item criteria__border'>
+                    <div className='criteria__type'>Type:</div>
+                    <div className='criteria__value'>
+                        <button className='criteria__value_button active'>Maxmization</button>
+                        <button className='criteria__value_button'>Minimization</button>
+                    </div>
+                </div>
+                <div className='criteria__item'>
+                    <div className='criteria__type'>Validation:</div>
+                    <div className='criteria__value'>
+                        <button className='criteria__value_button active'>Non-negative</button>
+                        <button className='criteria__value_button'>Threshold</button>
+                    </div>
+                </div>
+            </div>
+            <div class="slidecontainer">
+                <input type="range" 
+                    min={minValue} 
+                    max={maxValue} 
+                    value={inputValue} 
+                    step='0.1'
+                    class="slider" 
+                    onInput={(e) => setInputValue(e.target.value)} />
+                <input type='number' 
+                    min='0' 
+                    max={maxValue} 
+                    value={minValue} 
+                    step='0.1'
+                    pattern='\d+(,\d{1})?'
+                    className='min'
+                    onInput={(e) => setMinValue(e.target.value)} />
+                <input type='number' 
+                    value={maxValue} 
+                    min={minValue} 
+                    step='0.1'
+                    pattern='\d+(,\d{1})?'
+                    className='max'
+                    onInput={(e) => setMaxValue(e.target.value)} />
+                <input type='number' 
+                    value={inputValue} 
+                    min={minValue} 
+                    step='0.1'
+                    pattern='\d+(,\d{1})?'
+                    className='value'
+                    onInput={(e) => setInputValue(e.target.value)} />
+            </div>
+            <button className='edit__criteria'>Edit Optimization Criteria</button>
         </div>
     )
 }
--- ui/src/components/PatternList/index.js
+++ ui/src/components/PatternList/index.js
@@ -1,9 +1,21 @@
+import React from 'react'
+import './style.css'
+
+const pattrens = ['pattern one', 'pattern two']
 
 const PatternList = () => {
+    const [activePattern, setActivePattern] = React.useState(0)
+
     return (
-        <aside className='sidebar'>
+        <aside className='sidebar patterns'>
             <h2>Templates Palette</h2>
-            <button>+ Create New Pattern</button>
+            <div className='patternList'>
+                {pattrens.map((item, i) => (
+                    i === activePattern ?  (<span className='active' key={item}>{item}</span>) 
+                    : (<span onClick={() => setActivePattern(i)} key={item}>{item}</span>)
+                ))}
+            </div>
+            <button className='button_createPattern'>+ Create New Pattern</button>
         </aside>
     )
 }
