f9241c5876ad5a29e44a6b9e38a7e6444e4909f9
Vadim Vasilenko <vadim.vasilenko@timelysoft.net>
2022-03-17 15:19:15 +0600

    Crud pattern

---
+++ Models/PatternModel.cs
@@ -0,0 +1,25 @@
+﻿namespace p_designer.Models
+{
+    public class PatternModel
+    {
+        public class Read
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Status { get; set; }
+        }
+
+        public class Create
+        {
+            public string Name { get; set; }
+            public string Status { get; set; }
+        }
+
+        public class Update
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Status { get; set; }
+        }
+    }
+}
---
+++ Services/PatternService.cs
@@ -0,0 +1,44 @@
+﻿using Mapster;
+using Microsoft.EntityFrameworkCore;
+using p_designer.entities;
+using p_designer.Models;
+
+namespace p_designer.services
+{
+    public class PatternService
+    {
+        private PDesignerContext context { get; set; }
+        public PatternService(PDesignerContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<PatternModel.Read> ReadAsync(int id)
+        {
+            return await context.Patterns
+                .ProjectToType<PatternModel.Read>()
+                .SingleAsync(p => p.Id == id);
+        }
+
+        public async Task CreateAsync(PatternModel.Create patternModel)
+        {
+            var pattern = patternModel.Adapt<Pattern>();
+            await context.AddAsync(pattern);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(PatternModel.Update patternModel)
+        {
+            var pattern = patternModel.Adapt<PatternModel.Update>();
+            context.Update(pattern);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var pattern = await context.Patterns.FindAsync(id);
+            context.Remove(pattern);
+            await context.SaveChangesAsync();
+        }
+    }
+}
