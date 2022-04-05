cb4d9c6e05235663849c8515a714d86f5581b9d9
Vadim Vasilenko <vadim.vasilenko@timelysoft.net>
2022-04-05 18:44:23 +0600

    Реализовал CRUD типов компонентов

---
+++ api/Models/ComponentTypeModel.cs
@@ -0,0 +1,34 @@
+﻿namespace p_designer.Models
+{
+    public class ComponentTypeModel
+    {
+        public class Read
+        {
+            public class Short
+            {
+                public int Id { get; set; }
+                public string Name { get; set; }
+            }
+
+            public class Long
+            {
+                public int Id { get; set; }
+                public int LibraryId { get; set; }
+                public string Name { get; set; }
+            }
+        }
+
+        public class Create
+        {
+            public int LibraryId { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Update
+        {
+            public int Id { get; set; }
+            public int LibraryId { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
