e36f7912aa68d5f3471ef20c365367fd34af471e
Makaraha <88317594+Makaraha@users.noreply.github.com>
2022-04-07 15:27:39 +0600

    Изменил модель компонентов

--- api/Entities/Component.cs
+++ api/Entities/Component.cs
@@ -8,24 +8,33 @@ namespace p_designer.Entities
         public Component()
         {
             AttributeValues = new HashSet<AttributeValue>();
-            Aspects = new HashSet<AspectLevel>();
-            ComponentLevels = new HashSet<ComponentLevel>();
-            Libraries = new HashSet<Library>();
             Projects = new HashSet<Project>();
         }
 
         [Key]
         public int Id { get; set; }
+
         [ForeignKey(nameof(LifecycleStatus))]
         public int LifeCycleStatusId { get; set; }
+
+        [ForeignKey(nameof(ComponentType))]
+        public int? ComponentTypeId { get; set; }
+
+        [ForeignKey(nameof(Library))]
+        public int LibraryId { get; set; }
+
+        [ForeignKey(nameof(AspectLevel))]
+        public int CategoryId { get; set; }
+
         [Required]
         public string Name { get; set; }
-        public ComponentType Type { get; set; } // Тип должен содержаться в библиотеке
+
+        public ComponentType ComponentType { get; set; } // Тип должен содержаться в библиотеке
+        public Library Library { get; set; }
+        public AspectLevel Category { get; set; }
         public LifecycleStatus LifecycleStatus { get; set; }
         public ICollection<AttributeValue> AttributeValues { get; set; }
-        public ICollection<AspectLevel> Aspects { get; set; }
-        public ICollection<ComponentLevel> ComponentLevels { get; set; }
-        public ICollection<Library> Libraries { get; set; }
+        // public ICollection<ComponentLevel> ComponentLevels { get; set; }
         public ICollection<Project> Projects { get; set; }
     }
 }
---
+++ api/Services/ComponentService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using p_designer.Entities;
+using p_designer.Models;
+
+namespace p_designer.Services
+{
+    public class ComponentService
+    {
+        private PDesignerContext context { get; set; }
+        
+        public ComponentService(PDesignerContext context)
+        {
+            this.context = context;
+        }
+    }
+}
